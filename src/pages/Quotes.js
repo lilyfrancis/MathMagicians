bbb01d70dd93e2b25cd60bbb699d7fa0a2934f5e
Lilian Francis <ifnotlily@yahoo.com>
2021-11-04 15:31:25 +0100

    fix linters

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -4,10 +4,10 @@ const Quotes = () => (
   <section className="container-quotes">
     <h1>
       <q>
-      Mathematics as an expression of the human mind reflects the active will, 
-      the contemplative reason, and the desire for aesthetic perfection. 
-      Its basic elements are logic and intuition, analysis and construction, 
-      generality and individuality.. — Marston
+        Mathematics are the result of mysterious powers which no one
+        understands, and which the unconscious recognition of beauty must play
+        an important part. Out of an infinity of designs a mathematician chooses
+        one pattern for beauty’s sake and pulls it down to earth. — Marston
         Morse.
       </q>
     </h1>
